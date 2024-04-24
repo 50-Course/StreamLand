8ad5228d75f5b6497e115b5492773a5096a3aaf3
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-04-24 05:00:07 +0100

    update working build

--- streamland/src/main/java/com/erosoft/App.java
+++ streamland/src/main/java/com/erosoft/App.java
@@ -1,12 +1,17 @@
 package com.erosoft;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App {
+    @RequestMapping("/")
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        run(args);
+        SpringApplication.run(App.class, args);
     }
 }
